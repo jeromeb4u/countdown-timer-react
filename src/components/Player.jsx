0121bc40bcfc291c6eb82841f4d5d09da9165db3
Jerome <jeromeb4u@gmail.com>
2025-11-05 23:21:57 +0530

    Added the name change functionality in Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  function handleChange(event) {
+    setSubmitted(false); //Set it to false to stop the text from changing on every keystroke
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
